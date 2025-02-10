f864bd4beb58b631223c5ce5895d081c0235ee33
Yurii Zhupikov <webdev56@gmail.com>
2025-02-10 16:40:21 +0300

    fix: default aws s3 bucket

--- src/lib/aws-s3.ts
+++ src/lib/aws-s3.ts
@@ -1,6 +1,6 @@
 import { S3Client, PutObjectCommand, DeleteObjectCommand } from '@aws-sdk/client-s3'
 
-export const s3 = new S3Client({ region: process.env.AWS_REGION || 'us-east-1' })
+export const s3 = new S3Client({ region: process.env.AWS_REGION })
 
 /**
  * @param {string} bucketName
